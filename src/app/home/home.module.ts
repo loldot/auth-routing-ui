a7e5c28d050d2a3eaded85ece3563c9ce4c68418
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2018-03-02 11:10:49 +0100

    add home module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainmenuComponent]
+})
+export class HomeModule { }
---
+++ src/app/home/mainmenu/mainmenu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainmenu',
+  template: `
+    <p>
+      mainmenu works!
+    </p>
+  `,
+  styles: []
+})
+export class MainmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
