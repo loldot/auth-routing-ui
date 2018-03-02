bfb18faff807d90d5060bd63fb8b33e1aa55890d
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2018-03-02 11:10:03 +0100

    add submodules

---
+++ src/app/computers/computers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComputersRoutingModule } from './computers-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ComputersRoutingModule
+  ],
+  declarations: []
+})
+export class ComputersModule { }
